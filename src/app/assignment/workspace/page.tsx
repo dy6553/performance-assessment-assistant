8c7e3821209a0332c60722bd1fa9d81c2d246d84
dy6553 <yuni100106yuni@gmail.com>
2026-08-30 06:44:51 +0900

    feat: add assignment workspace route

---
+++ src/app/assignment/workspace/page.tsx
@@ -0,0 +1,5 @@
+import { AssessmentWizard } from "@/features/assessment/assessment-wizard";
+
+export default function AssignmentWorkspacePage() {
+  return <AssessmentWizard screen="workspace" />;
+}
