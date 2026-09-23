f2dd998a544eb03dc7877d3ac886c88d2602c833
dy6553 <yuni100106yuni@gmail.com>
2026-09-23 11:44:27 +0900

    pwa: use new vector app icon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -18,22 +18,10 @@ export default function manifest(): MetadataRoute.Manifest {
     categories: ["education", "productivity"],
     icons: [
       {
-        src: "/icon-192.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "any",
-      },
-      {
-        src: "/icon-512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any",
-      },
-      {
-        src: "/icon-512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable",
+        src: "/icon.svg",
+        sizes: "any",
+        type: "image/svg+xml",
+        purpose: "any maskable",
       },
     ],
   };
